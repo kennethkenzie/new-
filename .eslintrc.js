a319526b52cd27e16384ac0c5727a3dbd6b9ed9e
kenneth <kennethkenzie48@gmail.com>
2025-05-25 20:08:36 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: "next",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "no-console": "off",
+    "@next/next/no-img-element": "off",
+    // add more rules here
+  },
+};
